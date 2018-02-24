3a4416fed8eb2b13d52c6b45ae544f3e19c285c3
Aleksey Kuznetsov <zummenix@gmail.com>
2018-02-24 16:36:50 +0500

    Add install/update ansible in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,11 @@ else
     echo "Install homebrew"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+if [ -x "$(command -v ansible)" ]; then
+    echo "Update ansible"
+    brew upgrade ansible
+else
+    echo "Install ansible"
+    brew install ansible
+fi

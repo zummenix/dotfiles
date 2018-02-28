faf65e8ac9b32e91265b21f0b4b4267d74069642
Aleksey Kuznetsov <zummenix@gmail.com>
2018-02-28 07:54:14 +0500

    Reset some annoying colors in the fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,20 @@ set PATH "$HOME/.cargo/bin" $PATH
 if status --is-interactive
     eval sh $HOME/.config/base16-shell/scripts/base16-ocean.sh
 end
+
+set fish_color_command white
+set fish_color_comment white
+set fish_color_cwd white
+set fish_color_cwd_root white
+set fish_color_end white
+set fish_color_error white
+set fish_color_history_current white
+set fish_color_match white
+set fish_color_param white
+set fish_color_redirection white
+set fish_color_valid_path white
+set fish_color_escape white
+set fish_color_quote white
+set fish_pager_color_description normal
+
+set fish_greeting ""

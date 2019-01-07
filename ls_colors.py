ef4e7cb4d16dea467403a080072eaa32956f6b19
Aleksey Kuznetsov <zummenix@gmail.com>
2019-01-07 17:36:23 +0500

    Convert ls_colors.py script to python 3

--- ls_colors.py
+++ ls_colors.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 fg = '\033[38;5;'
 bg = '\033[48;5;'
 
-for i in range( 0, 256):
+for i in range(0, 256):
     n = str(i)
     fgstr = fg + n + 'm' + n
     bgstr = bg + n + 'm' 'XXXXX'
-    print fgstr, bgstr, '\033[0m'
+    print(fgstr, bgstr, '\033[0m')

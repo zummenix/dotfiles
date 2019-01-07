bcf171a4ac19b3cf1d6af209394e842a7f76d865
Aleksey Kuznetsov <zummenix@gmail.com>
2019-01-07 20:00:41 +0500

    Add an unit test for the color formatting in ls_colors.py

--- ls_colors.py
+++ ls_colors.py
@@ -3,8 +3,15 @@
 fg = '\033[38;5;'
 bg = '\033[48;5;'
 
+def format_color(index):
+    n = str(index)
+    fgstr = fg + n + 'm' + n + '\t'
+    bgstr = bg + n + 'm' + 'XXXXX'
+    return fgstr + bgstr + '\033[0m'
+
+def test_format_color():
+    assert format_color(5) == "\033[38;5;5m5\t\033[48;5;5mXXXXX\033[0m"
+    assert format_color(128) == "\033[38;5;128m128\t\033[48;5;128mXXXXX\033[0m"
+
 for i in range(0, 256):
-    n = str(i)
-    fgstr = fg + n + 'm' + n
-    bgstr = bg + n + 'm' 'XXXXX'
-    print(fgstr, bgstr, '\033[0m')
+    print(format_color(i))

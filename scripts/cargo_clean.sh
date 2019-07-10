c814111c5b36bd094c90cae3a3a8891ea18ecc7a
Aleksey Kuznetsov <zummenix@gmail.com>
2019-07-10 21:12:52 +0500

    Add a script to clean cargo projects

---
+++ scripts/cargo_clean.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+run_clean() {
+    DIR=$(dirname "$1")
+    du -hd0 "$DIR"
+    pushd "$DIR" > /dev/null && cargo clean
+    popd > /dev/null
+    du -hd0 "$DIR"
+}
+export -f run_clean
+
+find . -depth 2 -name "Cargo.toml" -print0 | xargs -0 -n 1 -I{} bash -c "run_clean {}"
+

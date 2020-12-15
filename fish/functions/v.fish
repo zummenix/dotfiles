add5fe2956e0215a64db54cfeaff0613312543fe
Aleksey Kuznetsov <zummenix@gmail.com>
2020-12-15 21:08:10 +0500

    Open either a new instance of neovim or a background instance

--- fish/functions/v.fish
+++ fish/functions/v.fish
@@ -1,3 +1,3 @@
-function v --description 'Shortcut for neovim'
-    nvim $argv
+function v --description 'Shortcut for neovim with background jobs support'
+    jobs | grep -ie "nvim" | head -n1 | cut -f 2 - | fg || nvim $argv
 end

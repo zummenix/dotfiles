147bc49dbe1db3b82411539ac7c25c8a24529395
Aleksey Kuznetsov <zummenix@gmail.com>
2022-05-23 23:25:45 +0500

    Get rid of right prompt by integrating its info to the left prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,8 +1,33 @@
 function fish_prompt --description 'Write out the prompt'
-    if not test $status -eq 0
+    set -l last_status $status
+    set -l line_color 778
+    set -l git_branch (git rev-parse --abbrev-ref HEAD 2> /dev/null)
+
+    set_color $line_color
+    echo ""
+    echo -n "in  "(prompt_pwd)"  "
+
+    if test -n "$git_branch"
+        set -l git_status (git status --porcelain 2> /dev/null)
+        echo -n "git "
+        if test -z "$git_status"
+            set_color green --bold
+            echo -n "•"
+        else
+            set_color red --bold
+            echo -n "•"
+        end
+        set_color reset
+        set_color $line_color
+        echo -n " [$git_branch]"
+    end
+
+    echo ""
+
+    if not test $last_status -eq 0
         set_color red
     else
         set_color 778
     end
-    echo -n 'λ '
+    echo -n "λ "
 end

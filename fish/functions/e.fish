299a7062d0775c85fd6c6ce112bc7dec6b116321
Aleksey Kuznetsov <zummenix@gmail.com>
2022-11-19 23:21:46 +0500

    Change e shortcut to open helix

--- fish/functions/e.fish
+++ fish/functions/e.fish
@@ -1,3 +1,3 @@
-function e --description 'Shortcut to open an editor (neovim) with background jobs support'
-    jobs | grep -ie "nvim" | head -n1 | cut -f 2 - | fg || nvim $argv
+function e --description 'Shortcut to open my favorite editor'
+    hx $argv
 end

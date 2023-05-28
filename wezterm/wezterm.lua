4b890bf50c58e323347cd1bc1f146942b802066c
Aleksey Kuznetsov <zummenix@gmail.com>
2023-05-28 09:37:54 +0500

    Correct font's Bold style in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,6 +3,15 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font = wezterm.font('Source Code Pro')
+config.font_rules = {
+  {
+    intensity = 'Bold',
+    italic = false,
+    font = wezterm.font_with_fallback({
+      { family = "Source Code Pro", weight = "Bold" },
+    })
+  },
+}
 
 config.colors = {
   cursor_bg = '#A7ADBA',

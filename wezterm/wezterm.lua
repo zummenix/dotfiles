dd9c98b07403d68cc99f34327e5f43d6bfbc33b5
Aleksey Kuznetsov <zummenix@gmail.com>
2023-05-29 20:45:35 +0500

    Setup wezterm's tab bar colors

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -16,6 +16,38 @@ config.font_rules = {
 config.colors = {
   cursor_bg = '#A7ADBA',
   cursor_fg = '#2B303B',
+
+  tab_bar = {
+    inactive_tab_edge = '#343D46',
+    active_tab = {
+      bg_color = '#A3BE8C',
+      fg_color = '#2B303B',
+    },
+    inactive_tab = {
+      bg_color = '#2B303B',
+      fg_color = '#A7ADBA',
+    },
+    inactive_tab_hover = {
+      bg_color = '#2B303B',
+      fg_color = '#C0C5CE',
+    },
+
+    -- The new tab button that let you create new tabs
+    new_tab = {
+      bg_color = '#2B303B',
+      fg_color = '#A7ADBA',
+    },
+    new_tab_hover = {
+      bg_color = '#2B303B',
+      fg_color = '#C0C5CE',
+    },
+  },
+}
+
+config.tab_bar_at_bottom = true
+config.window_frame = {
+  active_titlebar_bg = '#343D46',
+  inactive_titlebar_bg = '#4F5B66',
 }
 
 return config
